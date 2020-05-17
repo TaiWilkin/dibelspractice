a72a85f9e08c52fb6f8c85e96d6f83e151535906
Tai Corraggio <taicorraggio@gmail.com>
2020-05-17 16:08:05 -0400

    Updates

---
+++ precache-manifest.e08850a4d3cfc6edaf899b23faefa510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1366ecdb8ac6d6a227773130ee867ac8",
+    "url": "/dibelspractice/index.html"
+  },
+  {
+    "revision": "5337bb474c9c304f18ca",
+    "url": "/dibelspractice/static/css/main.2796544f.chunk.css"
+  },
+  {
+    "revision": "eb4cff3c1a441aced2dd",
+    "url": "/dibelspractice/static/js/2.c1e6bf97.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/dibelspractice/static/js/2.c1e6bf97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5337bb474c9c304f18ca",
+    "url": "/dibelspractice/static/js/main.985216b7.chunk.js"
+  },
+  {
+    "revision": "fbdec92050af123e611f",
+    "url": "/dibelspractice/static/js/runtime-main.fdf59433.js"
+  }
+]);
